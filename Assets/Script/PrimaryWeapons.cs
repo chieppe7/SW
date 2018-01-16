c31d1a6393f6c9bc029d4b59723e7f30dd097754
Chieppe <chieppe2142@gmail.com>
2018-01-16 15:34:48 -0200

    first Commit
    yay

---
+++ Assets/Script/PrimaryWeapons.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryWeapons : MonoBehaviour {
+
+	public int type;
+	private int power;
+	private float cooldown=0.5f;
+	private float delay=0.1f;
+	private float delaya;
+	private float cycle=0.3f;
+	public Transform[] t;
+	public Rigidbody proj;
+	private bool shooting;
+
+	// Use this for initialization
+	void Start () {
+		power = gameObject.GetComponent<Ship>().power;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1") && power>0 && !shooting){
+			StartCoroutine(FireDelay(0,0));
+		}
+	}
+
+	void Fire(Transform T) {
+		Rigidbody Clone = (Rigidbody) Instantiate(proj, T.position, T.rotation);
+		Clone.velocity = Clone.transform.TransformDirection(Vector3.forward * 30f);
+	}
+
+	IEnumerator FireDelay(int i, int j) {
+		shooting=true;
+		Fire(t[i++]);
+		if(i>=t.Length){
+			delaya = cycle;
+			i=0;
+			j++;
+		}
+		else
+			delaya = delay;
+		yield return new WaitForSeconds(delaya);
+		if(j<power)
+			StartCoroutine(FireDelay(i,j));
+		else
+			StartCoroutine(Cooldown());
+	}
+
+	IEnumerator Cooldown(){
+		yield return new WaitForSeconds(cooldown);
+		shooting=false;
+	}
+}
---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	//ship properties
+	public string name;
+	public int hull;
+	public int shield;
+	public int power;
+	public float engine;
+	public float Speed;
+	public float turnSpeed;
+
+	private Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+		rig = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+
+	void takeDamage(int dmg) {
+		hull-=dmg;
+		if (hull<=0)
+			die();
+	}
+
+	void die() {
+		
+	}
+
+	void move() {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		if(h*h+v*v<0.1){
+			return;
+		}
+		//anim.SetBool("IsMoving",true);
+
+		Vector3 movement = new Vector3(h, 0.0f, v);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), turnSpeed);
+		rig.AddForce(movement*engine, ForceMode.Acceleration);
+		rig.velocity=Vector3.ClampMagnitude(rig.velocity, Speed);
+	}
+}
